05c08f47ed7be341a68e2a8e700aa646914e340c
earthcharmeddec <earthcharmeddec@gmail.com>
2022-10-05 21:18:09 +0200

    Job board Flask Setup #1

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,11 @@
 import flask
+from flask import Flask
 from flask import render_template
 
 app = flask.Flask(__name__)
 
 
+@app.route('/')
 @app.route('/jobs')
 def jobs():
     return flask.render_template('index.html')
