a4fd88572ce3e4ae209365d2c5f2701070bd5323
earthcharmeddec <earthcharmeddec@gmail.com>
2022-10-06 21:48:08 +0200

    Job board Flask Setup #1

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,5 @@
 import flask
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template, url_for
 
 app = flask.Flask(__name__)
 
@@ -9,3 +8,8 @@ app = flask.Flask(__name__)
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
+
+
+url_for("jobs", filename= "../static/css/bulma.min.css")
+url_for("jobs", filename2= "../static/css/app.css")
+url_for("jobs", filename2= "https://use.fontawesome.com/releases/v5.2.0/css/all.css")
